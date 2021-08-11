2b07308db99882b23f869a4c37ea7839738bc0f2
Benedikt Siebert <BenediktS@ppedv.de>
2021-08-11 11:48:32 +0200

    9

---
+++ Uebungen/Modul009_Fragen/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Modul009_Fragen
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Form form = new Form(5.4, 9.7);
+            Kreis kreis = new Kreis(5);
+            Zylinder zylinder = new Zylinder(6.9, 2.4);
+            Kugel kugel = new Kugel(7);
+
+            Form[] formen = new Form[] { form, kreis, zylinder, kugel };
+
+            foreach (var item in formen)
+            {
+                Console.WriteLine(item.ToString());
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
